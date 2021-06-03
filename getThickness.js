93196d4e451f5a94bf71e7355a44c2781407dcda
imamorn <84733029+imamorn@users.noreply.github.com>
2021-06-03 17:29:19 +0700

    Update getThickness.js

--- getThickness.js
+++ getThickness.js
@@ -15,14 +15,14 @@ function thickness() {
 					
 		// Add name to row
 		var x = document.getElementById("myTable").rows[1].cells;
-  		x[1].innerHTML = JSON.stringify(response.data[0].positions[0].name);
-		x[2].innerHTML = JSON.stringify(response.data[0].positions[1].name);
-		x[3].innerHTML = JSON.stringify(response.data[0].positions[2].name);
-		x[4].innerHTML = JSON.stringify(response.data[0].positions[3].name);
-		x[5].innerHTML = JSON.stringify(response.data[0].positions[4].name);
-		x[6].innerHTML = JSON.stringify(response.data[0].positions[5].name);
-		x[7].innerHTML = JSON.stringify(response.data[0].positions[6].name);
-		x[8].innerHTML = JSON.stringify(response.data[0].positions[7].name);
+  		x[2].innerHTML = JSON.stringify(response.data[0].positions[0].name);
+		x[3].innerHTML = JSON.stringify(response.data[0].positions[1].name);
+		x[4].innerHTML = JSON.stringify(response.data[0].positions[2].name);
+		x[5].innerHTML = JSON.stringify(response.data[0].positions[3].name);
+		x[6].innerHTML = JSON.stringify(response.data[0].positions[4].name);
+		x[7].innerHTML = JSON.stringify(response.data[0].positions[5].name);
+		x[8].innerHTML = JSON.stringify(response.data[0].positions[6].name);
+		x[9].innerHTML = JSON.stringify(response.data[0].positions[7].name);
 
 		let newLen = response.data.length;
 		var table = document.getElementById("myTable");
@@ -41,16 +41,18 @@ function thickness() {
   				var cell6 = row.insertCell(6);
   				var cell7 = row.insertCell(7);
   				var cell8 = row.insertCell(8);
+				var cell9 = row.insertCell(9);
 				// Add thickness to row
-				cell0.innerHTML = JSON.stringify(response.data[i].timestamp);;
-  				cell1.innerHTML = JSON.stringify(response.data[i].positions[0].thickness);
-  				cell2.innerHTML = JSON.stringify(response.data[i].positions[1].thickness);
-				cell3.innerHTML = JSON.stringify(response.data[i].positions[2].thickness);
-				cell4.innerHTML = JSON.stringify(response.data[i].positions[3].thickness);
-				cell5.innerHTML = JSON.stringify(response.data[i].positions[4].thickness);
-				cell6.innerHTML = JSON.stringify(response.data[i].positions[5].thickness);
-				cell7.innerHTML = JSON.stringify(response.data[i].positions[6].thickness);
-				cell8.innerHTML = JSON.stringify(response.data[i].positions[7].thickness);
+				cell0.innerHTML = i;
+				cell1.innerHTML = JSON.stringify(response.data[i].timestamp);;
+  				cell2.innerHTML = JSON.stringify(response.data[i].positions[0].thickness);
+  				cell3.innerHTML = JSON.stringify(response.data[i].positions[1].thickness);
+				cell4.innerHTML = JSON.stringify(response.data[i].positions[2].thickness);
+				cell5.innerHTML = JSON.stringify(response.data[i].positions[3].thickness);
+				cell6.innerHTML = JSON.stringify(response.data[i].positions[4].thickness);
+				cell7.innerHTML = JSON.stringify(response.data[i].positions[5].thickness);
+				cell8.innerHTML = JSON.stringify(response.data[i].positions[6].thickness);
+				cell9.innerHTML = JSON.stringify(response.data[i].positions[7].thickness);
 			}
 		}
 
